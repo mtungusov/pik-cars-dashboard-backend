c12ed27b57504f9f20c799e52ebafb8116e18b71
TM <tm@macadmin.ru>
2017-03-14 17:55:43 +0300

    Update NSI in tread

--- app.rb
+++ app.rb
@@ -9,20 +9,38 @@ puts "Jruby: #{ENV['RUBY_VERSION']}"
 require 'lib/settings'
 puts "Namespace: #{Settings::Config.namespace}"
 puts "App: #{Settings::ALL.app_name}"
+puts "Update NSI in: #{Settings::ALL.perion_nsi_update} sec"
 
 
 require 'lib/ext_service'
 require 'lib/storage'
-$conn_write = Storage.client(db_file: File.join(Settings::CUR_DIR, 'db', 'db.sqlite')).open
 
 require 'lib/updater'
-Updater.update_nsi $conn_write
 
-require 'pry'
-binding.pry
+require 'thread'
+Thread.abort_on_exception = true
 
-# require 'thread'
-# Thread.abort_on_exception = true
+Thread.new do
+  $conn_write = Storage.client(db_file: File.join(Settings::CUR_DIR, 'db', 'db.sqlite')).open
+  loop do
+    Updater.update_nsi $conn_write
+    sleep Settings::ALL.perion_nsi_update
+  end
+end
+
+sleep
+
+at_exit {
+  puts "Terminate:at_exit:start"
+  Storage.client.close $conn_write
+  sleep 1
+  puts "Terminate:at_exit:end"
+  exit!
+}
+
+# require 'pry'
+# binding.pry
+
 
 # todo
 # Retrive data from API
--- lib/updater.rb
+++ lib/updater.rb
@@ -2,13 +2,30 @@ module Updater
   module_function
 
   def update_nsi(connection)
+    connection.setAutoCommit(false)
     _groups(connection)
-    # rules
-    # zones
-    # trackers
+    _rules(connection)
+    _zones(connection)
+    _trackers(connection)
+    connection.commit
+  rescue => e
+    connection.rollback
+    puts e.to_s
   end
 
   def _groups(connection)
     ExtService::Navixy.groups.each { |item| Storage.upsert_into(connection, 'groups', item) }
+  end
+
+  def _rules(connection)
+    ExtService::Navixy.rules.each { |item| Storage.upsert_into(connection, 'rules', item) }
+  end
+
+  def _zones(connection)
+    ExtService::Navixy.zones.each { |item| Storage.upsert_into(connection, 'zones', item) }
+  end
+
+  def _trackers(connection)
+    ExtService::Navixy.trackers.each { |item| Storage.upsert_into(connection, 'trackers', item) }
   end
 end
